c95302a046c74854c5148e41891a5daad015218f
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2023-01-01 22:26:55 -0500

    Initial Commit

---
+++ contacts-core-data/contacts_core_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  contacts_core_dataApp.swift
+//  contacts-core-data
+//
+//  Created by Deonte Kilgore on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct contacts_core_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
